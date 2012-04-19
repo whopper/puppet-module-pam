2cdec902f4241bc712e8a3be6b5bd3b39ecb27b3
Reid Vandewiele <marut@cat.pdx.edu>
2012-04-19 10:40:07 -0700

    Realign code to puppet style guide
    Just some cleanup and reallignment

--- manifests/access.pp
+++ manifests/access.pp
@@ -1,33 +1,27 @@
 define pam::access (
-  $ensure     = "present",
-  $permission = undef,
-  $entity     = undef,
-  $origin     = undef,
-  $priority   = 10,
+  $permission,
+  $entity,
+  $origin,
+  $ensure     = present,
+  $priority   = '10'
 ) {
   include pam
 
-  case $osfamily {
-    "Debian": {
-    if $permission == undef {
-      fail("Permission (+ or -) must be provided")
-    } elsif $entity == undef {
-      fail("Entity must be provided")
-    } elsif $origin == undef {
-      fail("Origin must be provided")
-    }
-
-    realize Concat["/etc/security/access.conf"]
-
-    concat::fragment { "$entity":
-      ensure  => $ensure,
-      target  => "/etc/security/access.conf",
-      content => "${permission}:${entity}:${origin}\n",
-      order   => $priority,
-    }
+  if ! ($osfamily in ['Debian']) {
+    fail("pam::access does not support osfamily $osfamily")
   }
-    default: {
-      fail("Module does not support $osfamily")
-    }
+
+  if ! ($permission in ['+', '-']) {
+    fail("Permission must be + or - ; recieved $permission")
   }
+
+  realize Concat['/etc/security/access.conf']
+
+  concat::fragment { $entity:
+    ensure  => $ensure,
+    target  => '/etc/security/access.conf',
+    content => "${permission}:${entity}:${origin}\n",
+    order   => $priority,
+  }
+
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,15 @@
 class pam {
   include concat::setup
 
- @concat { "/etc/security/access.conf":
-    owner  => "root",
-    group  => "root",
-    mode   => "0644",
+  @concat { '/etc/security/access.conf':
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
   }
 
- @concat { "/etc/security/limits.conf":
-    owner  => "root",
-    group  => "root",
-    mode   => "0644",
+  @concat { '/etc/security/limits.conf':
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
   }
 }
